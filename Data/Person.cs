e10e0a6138068fcf9d8f5b56076197cbfc01e6e8
jsakamotoIGJP <jsakamoto@infragistics.com>
2023-08-07 14:42:58 +0900

    初回コミット

---
+++ Data/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace KB9890_BlazorWasmApp1.Data;
+
+public class Person
+{
+    public int ID { get; set; }
+    public string FamilyName { get; set; } = String.Empty;
+    public string GivenName { get; set; } = String.Empty;
+    public string Prefecture { get; set; } = String.Empty;
+    public string City { get; set; } = String.Empty;
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using KB9890_BlazorWasmApp1;
+using KB9890_BlazorWasmApp1.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor();
+builder.Services.AddSingleton<PeopleDataService>();
+
+await builder.Build().RunAsync();
